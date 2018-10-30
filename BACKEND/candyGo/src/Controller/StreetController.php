60c35b7d5e9861514d9608b9a5d2e6bf311f9e2a
Julie Delmas <julie.delmas33@gmail.com>
2018-10-30 22:28:51 +0100

    Add use request and adress.sql.

--- BACKEND/candyGo/src/Controller/StreetController.php
+++ BACKEND/candyGo/src/Controller/StreetController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class StreetController extends AbstractController
 {
