7761454d7d1242327bd8f700d7891aae222e08f2
Julie Delmas <julie.delmas33@gmail.com>
2018-10-31 07:47:00 +0100

    Making localisation's function.

--- BACKEND/candyGo/src/Controller/StreetController.php
+++ BACKEND/candyGo/src/Controller/StreetController.php
@@ -7,6 +7,7 @@ use App\Repository\StreetRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use GuzzleHttp\Client;
 
 class StreetController extends AbstractController
 {
@@ -25,6 +26,34 @@ class StreetController extends AbstractController
 
         // Response conversion to json
         return $response = $this->json($street);
+
+    }
+
+    /**
+     * @throws
+     * @Route("/localisation", name="localisation")
+     */
+    public function localisation()
+    {
+        $client = new Client([
+                'base_uri' => 'https://api-adresse.data.gouv.fr/reverse/',
+            ]
+        );
+
+        $search = '?lon=3.065&lat=50.630&type=street';
+
+        // Send a request
+        $response = $client->request('GET', $search );
+
+        // Create Response
+        $body = $response->getBody();
+        $location = $body->getContents();
+
+        $response = new Response();
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        // Response conversion to json
+        return $response = $this->json($location);
 
     }
 }
