05f664819af85de7d4a497208198d6b3db8c001f
Thomas SY <sy.thomas.pro@gmail.com>
2018-10-31 10:46:20 +0100

    Add "Function doctrine vs json 2 StreetController".

--- BACKEND/candyGo/config/bundles.php
+++ BACKEND/candyGo/config/bundles.php
@@ -7,4 +7,5 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
